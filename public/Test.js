fbc6ec0a1912b98e4498ca6676340891155e086e
Jahmai Scarlett <jahmai.scarlett@gmail.com>
2018-07-12 14:43:46 -0500

    heruko addin

---
+++ public/Test.js
@@ -0,0 +1,159 @@
+<!doctype html>
+<html>
+  
+  <head> 
+  <title> The Visionaries </title>
+  </head>
+  
+    <h1> The People of Change </h1>
+    <br>
+    <p> Click links to skip to the following countries </p>
+    <ul> 
+      <li> <a href="#Jamaica"> Jamaican Section </a>  </li>
+      <li> <a href= "#United States of America" > United States of America Section </a> </li>
+      <li> <a href= "#Germany"> German Section </a> </li>
+       
+    </ul> 
+    <br>
+    <p> Every generation people have rallied behind a person of power or influence</p>
+    
+  
+    <table>
+     <tr>
+       <th> Jamaica </th>
+       <th> United States of America </th>
+       <th> Germany </th>
+     </tr>
+      
+     <tr>
+       <td> <a href="#Sam Sharpe">Sam Sharpe</a> </td>
+       <td> <a href="#MLK"> Martin Luther King Jr.</a> </td>
+       <td> Adolf Hitler </td>
+     <tr>
+        
+     <tr>
+       <td> <a href="#Nanny of the Maroons">Nanny of the Maroons </a></td>
+       <td> Abraham Lincon </td>
+       <td> Angella Merkle </td>
+     <tr>
+    
+    </table>
+    <h1> <a name= "Jamaica"> Jamaica </a> </h1>
+     <br>
+     <br>
+    <h2><a name="Sam Sharpe"> Sam Sharpe </a> </h2>
+    <br>
+    <!-- This for none other than test--> 
+    <p> National Hero of Jamaica</p>
+    <h3> Things Sam Sharpe achievved include</h3>
+    <ul>
+    <li> aided in the freedon of his people </li>
+     <li> Allowed the gateway for more succcesful rebellions</li>
+    
+    </ul> 
+    <h1> Picture of Sam Sharpe </h1> 
+    <p> <img src="Image.jpg" width="200" height="200"></p>
+<br>
+<br>
+<br>
+<br>
+    <p> To read more about this hero, click on link provided
+    <a href=https://jis.gov.jm/information/heroes/samuel-sharpe/> <!-- referencing another site outside of its own-->
+      Official Jamaican Page </a> <!-- What will come up instead of link-->
+  </p>
+<br>
+<br>
+<h2> <a name="Nanny of the Maroons">  Nanny of the Maroons  </a> </h2>
+<br>
+<br>
+<p> She is the only female national hero for this country and was very influencial to how the country is now. She has been involed in: </p>
+<ul>
+  <li> Using gurrila tactics which caused her people to be freed</li>
+  <li> Her bravery and leaadership allowed her people to believe freedom was a possiblitiy</li>
+    
+</ul>
+<h1> Picture of Nanny of the Maroons </h1>
+<p> <img src= "Image Nanny of Maroons.jpg" width="200" height="200" > </p>
+<p> To read more about this hero, click on link provided
+<a href= https://jis.gov.jm/information/heroes/nanny-of-the-maroons/>
+Official Jamaican page </a>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<h1> <a name= "United States of America"> United States of America </a> </h1>
+<br>
+<br>
+<p> This presigious country of late has had many issues of crime, slavery and racism which were prevelant in the past.
+The country had leaders to bring them out of such travesties.Here are a few of the people that allowed America to be
+successful country it is now
+<br>
+<br>
+<h2> <a name="MLK"> Martin Luther King Jr.(MLK)</a> </h2>
+<br>
+<br>
+<br>
+<p> He is the figure head for the positive coexistance between blacks and non- blacks as he paved a way in
+which acceptance is not only sought after but achieved</p>
+<p>Few of the things he achieved include
+<ul>
+<li>Allowed the mentality of freedom to set in</li>
+<li>Allowed black people to vote</li>
+<li>Made the existance of inter-racial relationships to happen</li>
+</ul>
+<h2> Picture of Martin Luther King Jr. </h2>
+<br>
+<br>
+<p>  <img src="Image MLK.jpg" width="200" height="200"> </p>
+<br>
+<p> To learn more of this hero,click on link provided
+ <a href="http://www.thekingcenter.org/> Official American Page </a> 
+<br>
+<br>
+<br>
+<br>
+<h1> <a name="Germany"> Germany </a> </h1>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<p> Please fill out the box below to tell us how we can improve the sight </p>
+<form action="" method="">
+  <textarea rows="20" cols="60"> </textarea> <br>
+  <input type="submit" value="Submit" >
+</form>
+
+<p> Sign up to membership of site below</p>
+<br>
+<br>
+<label for="firstname">First Name:       </label>
+<input type= "text" id=firstname name="" size="50">
+<br> <br>
+<label for ="lastname">Last Name:   </label>
+<input type="text" id=lastname name"" size="50">
+<br> <br>
+<label for ="email"> Email Address: </label>
+<input type="text" id=email name"" size="70">
+
+
+
+</body>  
+  
+</html>
