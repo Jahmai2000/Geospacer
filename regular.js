7648fac173ad0cd5125e536265dc149f6f56be08
Jahmai Scarlett <jahmai.scarlett@gmail.com>
2018-07-12 16:26:32 -0500

    mistakes rectified

--- regular.js
+++ regular.js
@@ -13,20 +13,16 @@ function changeText(){
 	//var secondParaText= paragraph[1].innerHTML;
 	//var addThem = paragraph[2].innerHTML = firstParaText + secondParaText;
 	
-	//var elemntH = document.createElement("h2")
-	//var main = document.getElementById("main");
-	//main.appendChild(elementH);
-	//var textH = document.createTextNode(" Rick Astley - (Never Gonna Give you up) ");
-	//elementH.appendChild(textH);
+	/*var elemntH = document.createElement("h2")
+	var main = document.getElementById("main");
+	main.appendChild(elementH);
+	var textH = document.createTextNode("The Sad Truth");
+	elementH.appendChild(textH);*/
 	
 	var element= document.createElement("p");
-	
 	var main = document.getElementById("main");
-	
 	main.appendChild(element);
-	
-	var text = document.createTextNode("We're no strangers to love. You know the rules and so do I. A full commitment's what I'm thinking of .You wouldn't get this from any other guy. I just wanna tell you how I'm feeling. Gotta make you understand. Never gonna give you up .Never gonna let you down .Never gonna run around and desert you .Never gonna make you cry. Never gonna say goodbye. Never gonna tell a lie and hurt you. We've known each other for so long. Your heart's been aching but. You're too shy to say");
-
+	var text = document.createTextNode(" Rooster is just a way of life");
 	element.appendChild(text);
 	
 	var pAttribute = document.createAttribute("id");
